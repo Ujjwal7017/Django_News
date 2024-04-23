45fdc34279883f35c02e1850b2336515c4b428a6
Ujjwal Shukla <ujjwal7017@gmail.com>
2024-04-23 10:38:20 +0530

    added requirements.txt file

--- newsproject/settings.py
+++ newsproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j*zav_#%vlc0dhkz9*yy^ht3nmg#=l9q0ce$zkq_(8=apz^*qb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
